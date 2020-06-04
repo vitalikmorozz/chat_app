56eda0e2a5a030b717823a911a4b76da4b366341
Vitalik Moroz <vitalik.moroz2@gmail.com>
2020-06-04 16:50:56 +0300

    implement alert messages when user connected/disconnected, refactor messages design

--- socket/chat.io.js
+++ socket/chat.io.js
@@ -1,15 +1,20 @@
 module.exports = (io) => {
 	io.on('connection', (socket) => {
-		console.log('Connected');
-		socket.on('msg:send', (data) => {
-			console.log(data);
+		let user = {};
+		socket.on('user:connecting', (data) => {
+			user = data.user;
+			console.log(`New user ${user.username} connected`);
+			io.emit('user:connected', { user });
+		});
 
+		socket.on('msg:send', (data) => {
 			socket.emit('msg:received', data);
 			socket.broadcast.emit('msg:received', data);
 		});
 
-		socket.on('disconnect', () => {
-			console.log('Disconnected');
+		socket.on('disconnecting', () => {
+			console.log(`${user.username} disconnected`);
+			socket.broadcast.emit('user:disconnected', { user });
 		});
 	});
 };
